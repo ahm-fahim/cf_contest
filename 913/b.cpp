cc68d3d6d784f1ff3be82beb43cc3c15dace6adf
ahm-fahim <contactahmfahim@gmail.com>
2023-12-05 23:06:17 +0600

    solved

--- 913/b.cpp
+++ 913/b.cpp
@@ -1,62 +1,58 @@
+/****~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~****\
+ |  Problems! Problems! Problems! I Don't Like It, I Avoid.             |
+ |  But Problems Likes Me! I can't Avoid!                               |
+ |                                                                      |
+ |  A.H.M. Fahim Morshed                                                |
+ |                                                                      |
+ |  ```Is a competitive programming enthusiast who is deeply            |
+ |  passionate about coding. Hailing from Bogura, Bangladesh,           |
+ |  his coding journey started at a young age, driven by a              |
+ |  fascination for solving intricate problems.                         |
+ |                                                                      |
+ |  Fahim has excelled in competitive programming, consistently         |
+ |  ranking among the top participants in various                       |
+ |  contests.Beyond competitions, he shares his                         |
+ |  expertise by mentoring and assisting fellow                         |
+ |  programmers on online forums.                                       |
+ |                                                                      |
+ |  His dedication to self-improvement, willingness to embrace new      |
+ |  challenges, and effective teamwork make him a valuable              |
+ |  asset to any project.                                               |
+ |                                                                      |
+ |  Fahim's love for coding, combined with his diverse interests,       |
+ |  reflects his well-rounded personality, making him an inspiring      |
+ |  figure in the programming community at Southeast University.```     |
+ |                                                                      |
+ |  Github   - ahm-fahim                                                |
+ |  Linkedin - https://www.linkedin.com/in/md-fahim-morshed-5b2126233/  |
+ |  Gmail    - contactahmfahim@gmail.com                                |
+\****~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~****/
+
 #include <bits/stdc++.h>
 using namespace std;
-
-vector<char> deleteLastLowercase(vector<char> &arr)
-{
-    for (int i = arr.size() - 1; i >= 0; --i)
-    {
-        if (islower(arr[i]))
-        {
-            arr.erase(arr.begin() + i);
-            break;
-        }
-    }
-    return arr;
-}
-
-vector<char> deleteLastUppercase(vector<char> &arr)
-{
-    for (int i = arr.size() - 1; i >= 0; --i)
-    {
-        if (isupper(arr[i]))
-        {
-            arr.erase(arr.begin() + i);
-            break;
-        }
-    }
-    return arr;
-}
-
 int main()
 {
-    int t;
-    cin >> t;
-
-    while (t--)
+    int n;
+    cin >> n;
+    while (n--)
     {
-        string s;
+        string s, ans = "";
+        int num1 = 0, num2 = 0;
         cin >> s;
-
-        vector<char> arr;
-        for (char i : s)
+        for (int i = s.size() - 1; i >= 0; --i)
         {
-            if (i == 'b')
-            {
-                arr = deleteLastLowercase(arr);
-            }
-            else if (i == 'B')
-            {
-                arr = deleteLastUppercase(arr);
-            }
+            if (s[i] == 'b')
+                num2++;
+            else if (s[i] == 'B')
+                num1++;
+            else if (s[i] >= 97 && s[i] <= 122 && num2 != 0)
+                num2--;
+            else if (s[i] >= 65 && s[i] <= 90 && num1 != 0)
+                num1--;
             else
-            {
-                arr.push_back(i);
-            }
+                ans += s[i];
         }
-
-        string result(arr.begin(), arr.end());
-        cout << result << "\n";
+        reverse(ans.begin(), ans.end());
+        cout << ans << endl;
     }
-
-    return 0;
 }
